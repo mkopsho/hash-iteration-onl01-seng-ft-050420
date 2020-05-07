635965b29ade34d6cb9db6b597e79576a905f285
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-07 17:56:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid, age|
+    if age <= 12
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    end
+  end
+end
