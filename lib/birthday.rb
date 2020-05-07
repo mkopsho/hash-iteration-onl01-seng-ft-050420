1be2e7deca72995fd100ef9256e5ed35ea40656e
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-07 17:54:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,8 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
-    puts "Happy birthday #{kid}! You are now #{age}"
+    puts "Happy birthday #{kid}! You are now #{age} years old!"
+  end
 end
 
 
